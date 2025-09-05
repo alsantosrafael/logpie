4a539f9eb28c9ba13dad04944602b23a281bd148
Rafael Almeida <rafael.profeng@gmail.com>
2025-09-05 19:24:10 -0300

    feat: Add logs file writing

--- logpie/logger.py
+++ logpie/logger.py
@@ -1,41 +1,56 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from enum import Enum
 import json
+import os
 import sys
-
 
 class LogLevel(str, Enum):
   INFO = "INFO"
   WARNING = "WARNING"
   ERROR = "ERROR"
   DEBUG = "DEBUG"
+  CRITICAL = "CRITICAL"
 
 class Logger:
   def __init__(self, level: LogLevel = LogLevel.INFO):
     self.level = level
     self.level_order = {
-      LogLevel.INFO: 10,
-      LogLevel.DEBUG: 20,
-      LogLevel.WARNING: 30,
-      LogLevel.ERROR: 40,
+        LogLevel.DEBUG: 10,
+        LogLevel.INFO: 20,
+        LogLevel.WARNING: 30,
+        LogLevel.ERROR: 40,
+        LogLevel.CRITICAL: 50,
     }
 
   def log(self, level: LogLevel, message: str, **kwargs):
     if self.level_order[level] < self.level_order[self.level]:
       return
-    
+
     log_entry = {
-      "timestamp": datetime.now().isoformat(),
+      "timestamp": datetime.now(timezone.utc).isoformat(),
       "level": level,
       "message": message,
       "extra": kwargs
     }
+    log_entry_json = json.dumps(log_entry) + "\n"
 
-    sys.stdout.write(json.dumps(log_entry) + "\n")
+    sys.stdout.write(log_entry_json)
+    self.write_file(log_entry_json)
 
   def info(self, msg, **kwargs): self.log(LogLevel.INFO, msg, **kwargs)
   def warning(self, msg, **kwargs): self.log(LogLevel.WARNING, msg, **kwargs)
   def error(self, msg, **kwargs): self.log(LogLevel.ERROR, msg, **kwargs)
   def debug(self, msg, **kwargs): self.log(LogLevel.DEBUG, msg, **kwargs)
+
+  def write_file(self, log):
+    print("DEBUG >>> entrando em write_file()")
+    try:
+      os.makedirs("logs", exist_ok=True)
+      today = datetime.now().strftime("%Y-%m-%d")
+      path = os.path.join("logs", f"{today}.log")
+      with open(path, "a") as file:
+          file.write(log)
+    except Exception as e:
+      print(f"Failed to write file: {e}")
 
 log = Logger(level=LogLevel.DEBUG)
